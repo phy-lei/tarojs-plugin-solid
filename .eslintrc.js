4a004866792b85f687972e4acdda7d8e657379d1
phy-lei <765373325@qq.com>
2024-01-22 17:23:50 +0800

    chore: 规则修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,35 +37,8 @@ module.exports = {
     'no-empty': 1,
     'prefer-spread': 0,
     'prefer-rest-params': 0,
-    'react/jsx-uses-vars': 1,
-    'react/prop-types': 0,
-    'react/no-find-dom-node': 0,
-    'react/no-unknown-property': 0,
     'quotes': [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
     'semi': [2, 'never'],
-    'simple-import-sort/imports': [
-      2,
-      {
-        groups: [
-          // Side effect imports.
-          ['^\\u0000'],
-          // Node.js builtins prefixed with `node:`.
-          ['^node:'],
-          // Packages.
-          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
-          ['^@?\\w'],
-          // Absolute imports and other imports such as Vue-style `@/foo`.
-          // Anything not matched in another group.
-          ['^'],
-          // Relative imports.
-          // Anything that starts with a dot.
-          ['^\\.'],
-          // Types Group
-          ['^node:.*\\u0000$', '^@?\\w.*\\u0000$', '(?<=\\u0000)$', '^\\..*\\u0000$'],
-        ],
-      },
-    ],
-    'simple-import-sort/exports': 2,
     'space-before-function-paren': [2, 'always'],
     'standard/no-callback-literal': 0,
   },
