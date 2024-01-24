8fe6083096f1d73b48cf193b2207b858c68f4475
phy-lei <765373325@qq.com>
2024-01-24 17:05:22 +0800

    feat: 类名及组件props测试

---
+++ packages/example/src/components/Counter.tsx
@@ -0,0 +1,23 @@
+import { View, Text, Button } from "@tarojs/components";
+import { createSignal } from "solid-js";
+import './counter.css'
+
+export default function Counter(props) {
+  const [count, setCount] = createSignal(props.count);
+
+  const add = () => {
+    setCount(count() + 1)
+  }
+
+  const sub = () => {
+    setCount(count() - 1)
+  }
+
+  return (
+    <View class="flex">
+      <Button onClick={add}>+</Button>
+      <Text>{count()}</Text>
+      <Button onClick={sub}>-</Button>
+    </View>
+  );
+}
--- packages/example/src/pages/index/index.tsx
+++ packages/example/src/pages/index/index.tsx
@@ -1,20 +1,11 @@
 import { View, Text, Button } from "@tarojs/components";
-import Taro from "@tarojs/taro";
+import { useLoad } from "@tarojs/taro";
+import Counter from "@/components/Counter";
 import { createSignal } from "solid-js";
 import "./index.css";
-import { useLoad } from "@tarojs/taro";
 
 export default function Index() {
-  const [count, setCount] = createSignal(0);
   const [color, setColor] = createSignal('red')
-
-  const handleClick = () => {
-    console.log('%c [ xxx ]', 'font-size:13px; background:pink; color:#bf2c9f;', Taro);
-    setCount(count() + 1)
-    Taro.navigateTo({
-      url: '/pages/about/index'
-    })
-  }
 
   useLoad(() => {
     console.log('%c [ xxx ]', 'font-size:13px; background:pink; color:#bf2c9f;', 123);
@@ -25,8 +16,7 @@ export default function Index() {
       <Text style={`color: ${color()}`}>Hello world! </Text>
       <Text style={{color: color()}}>Hello world2! </Text>
       <Button onClick={() => setColor('blue')}>set style</Button>
-      <View>{count()}</View>
-      <Button onClick={handleClick}>add</Button>
+      <Counter count={0}></Counter>
       <View>{Math.random()}</View>
     </View>
   );
