78f205890de11d0324518c347852db8bba4baeea
phy-lei <765373325@qq.com>
2024-01-24 11:01:07 +0800

    feat: 适配taro的hooks api

--- packages/example/src/pages/index/index.tsx
+++ packages/example/src/pages/index/index.tsx
@@ -2,6 +2,7 @@ import { View, Text, Button } from "@tarojs/components";
 import Taro from "@tarojs/taro";
 import { createSignal } from "solid-js";
 import "./index.css";
+import { useLoad } from "@tarojs/taro";
 
 export default function Index() {
   const [count, setCount] = createSignal(0);
@@ -14,6 +15,10 @@ export default function Index() {
       url: '/pages/about/index'
     })
   }
+
+  useLoad(() => {
+    console.log('%c [ xxx ]', 'font-size:13px; background:pink; color:#bf2c9f;', 123);
+  })
 
   return (
     <View className="index">
--- packages/tarojs-plugin-solid/src/loader-meta.ts
+++ packages/tarojs-plugin-solid/src/loader-meta.ts
@@ -1,7 +1,3 @@
-// import * as acorn from "acorn";
-// import * as walk from "acorn-walk";
-
-
 interface ILoaderMeta {
   importFrameworkStatement: string
   mockAppStatement: string
@@ -17,60 +13,6 @@ interface ILoaderMeta {
   modifyConfig?: (config: Record<string, any>, source: string) => void
 }
 
-// function addConfig(source) {
-//   const configsMap = {
-//     enableShareAppMessage: ["onShareAppMessage", "useShareAppMessage"],
-//     enableShareTimeline: ["onShareTimeline", "useShareTimeline"],
-//   };
-//   const ast = acorn.parse(source, {
-//     ecmaVersion: "latest",
-//     sourceType: "module",
-//   });
-
-//   const additionConfig: Record<string, any> = {};
-
-//   function check(name: string) {
-//     Object.keys(configsMap).forEach((configName) => {
-//       const apis: string[] = configsMap[configName];
-//       if (apis.includes(name)) {
-//         additionConfig[configName] = true;
-//       }
-//     });
-//   }
-
-//   walk.simple(ast, {
-//     FunctionExpression(node: any) {
-//       if (!node.id || !node.id.name) return;
-//       check(node.id.name);
-//     },
-//     FunctionDeclaration(node: any) {
-//       if (!node.id || !node.id.name) return;
-//       check(node.id.name);
-//     },
-//     CallExpression(node: any) {
-//       const { callee } = node;
-//       if (callee.type === "Identifier") {
-//         check(callee.name);
-//       } else if (callee.type === "MemberExpression") {
-//         if (callee.property.type === "Identifier") {
-//           check(callee.property.name);
-//         } else if (callee.property.type === "Literal") {
-//           check(callee.property.value);
-//         }
-//       }
-//       node.arguments.forEach((item) => {
-//         if (item.type === "Literal" && item.value) {
-//           check(item.value);
-//         }
-//       });
-//     },
-//   });
-
-//   return additionConfig;
-// }
-
-
-
 export function getLoaderMeta (): ILoaderMeta {
   return {
     importFrameworkStatement: ``,
@@ -81,8 +23,5 @@ export function getLoaderMeta (): ILoaderMeta {
     importFrameworkName: '',
     compatComponentImport: '',
     compatComponentExtra: '',
-    // modifyConfig(config, source) {
-    //   Object.assign(config, addConfig(source));
-    // },
   }
 }
