6490b01d84b1c4191d9f2230c8f9d32004c7ea0c
phy-lei <765373325@qq.com>
2024-02-02 17:00:31 +0800

    chore: 使用css module

--- packages/example/src/pages/index/index.tsx
+++ packages/example/src/pages/index/index.tsx
@@ -1,30 +1,30 @@
-import { View, Text, Button, Icon } from "@tarojs/components";
-import { useLoad } from "@tarojs/taro";
-import Counter from "@/components/Counter";
-import { createSignal } from "solid-js";
-import "./index.css";
+import { View, Text, Button, Icon } from '@tarojs/components'
+import { useLoad } from '@tarojs/taro'
+import Counter from '@/components/Counter'
+import { createSignal } from 'solid-js'
+import styles from './index.module.css'
 
 export default function Index() {
   const [color, setColor] = createSignal('red')
   const [cls, setCls] = createSignal('')
 
   useLoad(() => {
-    console.log('%c [ xxx ]', 'font-size:13px; background:pink; color:#bf2c9f;', 123);
+    console.log('%c [ xxx ]', 'font-size:13px; background:pink; color:#bf2c9f;', 123)
   })
 
   return (
-    <View className="index">
+    <View class="index">
       <View>
         <Text style={`color: ${color()}`}>Hello world! </Text>
       </View>
       <View>
         <Text class={cls()}>Hello world2! </Text>
       </View>
-      <Button onClick={() => setCls('bold')}>set class</Button>
+      <Button onClick={() => setCls(styles['bold'])}>set class</Button>
       <Button onClick={() => setColor('blue')}>set style</Button>
       <Counter count={0}></Counter>
       <View>{Math.random()}</View>
-      <Icon size='60' type='success' />
+      <Icon size="60" type="success" />
     </View>
-  );
+  )
 }
